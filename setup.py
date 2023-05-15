e4ba5f3c55a45a3cbe880549c87f413fe1a9ab50
shihan-ma <mmasss@sjtu.edu.cn>
2023-05-16 02:44:22 +0800

    update

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='BioMime',
+    name='try_package',
     version="1.0",
     author='Shihan Ma',
     author_email='mmasss1205@gmail.com',
-    description='BioMime is a package for simulating MUAPs from changes of physiological parameters',
 
     url='https://github.com/shihan-ma/BioMime',
 
